e0fe998bc1524d332d1e04d95ce6fd1aa592f154
nevets615 <surj316@yahoo.com>
2019-06-09 07:58:50 -0400

    changes knex

--- knexfile.js
+++ knexfile.js
@@ -3,25 +3,7 @@ const pg = require("pg");
 pg.defaults.ssl = true;
 
 
-
-//const dbConnection = process.env.DATABASE_URL || localPg;
-
 const dbSettings = {
-  client: "pg",
-  connection: process.env.DATABASE_URL,
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    directory: "./migrations",
-    tableName: "dbmigrations"
-  },
-  seeds: {
-    directory: "./seeds"
-  }
-};
-const dbSettings2 = {
   client: "pg",
   connection: process.env.DATABASE_URL,
   pool: {
@@ -38,8 +20,7 @@ const dbSettings2 = {
 };
 
 module.exports = {
-  development: dbSettings,
-  production: dbSettings2,
+  production: dbSettings,
   testing: {
     client: "sqlite3",
     connection: {
